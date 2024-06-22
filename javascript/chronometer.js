4f62dc4b3055dd69eaa5a51dd97952e7f30ebbbc
joseDuc <ppgq@live.com>
2024-06-22 10:15:35 +0200

    solve iteration x, y, z

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,35 +1,94 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.tiempoActual = 0;
+    this.idIntervalo = null;
+    //this.modoMilliseconds = false;
+
   }
 
   start(callback) {
     // ... your code goes here
+    if (!this.idIntervalo) {
+      console.log("start")
+      //this.tiempoActual = 0;
+      this.intervalo = 10;
+      this.idIntervalo = setInterval(() => {
+        {
+          this.tiempoActual += this.intervalo;
+        }
+        if (callback) callback();
+      }, this.intervalo);
+    }
   }
 
   getMinutes() {
     // ... your code goes here
+    let m = 0;
+    m = Math.floor((this.tiempoActual / 1000) / 60);
+    if (m > 59) m = 0;
+    return m;
   }
 
   getSeconds() {
     // ... your code goes here
+    let s = 0;
+    s = Math.floor((this.tiempoActual / 1000) % 60);
+    if (s > 59) s = 0;
+    return s;
+  }
+
+  getMilliseconds() {
+    // ... your code goes here
+    let ms = 0;
+    let sec = this.getSeconds();
+    let min = this.getMinutes();
+    ms = Math.floor(this.tiempoActual - (((min * 60) * 1000) + (sec * 1000)));
+    return ms;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+    let digit = "00";
+    if (value) {
+      let s= value.toString();
+      if (s.length < 2) {
+        digit = "0" +s;
+      } else if (s.length === 2) {
+        digit = s;
+      } else if (s.length > 2) {
+        digit = s[0] + s[1];
+      }
+    }
+    return digit;
   }
 
   stop() {
     // ... your code goes here
+    if (this.idIntervalo) {
+      clearInterval(this.idIntervalo);
+      this.idIntervalo = null;
+    };
   }
 
   reset() {
     // ... your code goes here
+    this.tiempoActual = 0;
+    if (this.idIntervalo) this.idIntervalo = null;
+    this.tiempoActual = 0;
   }
 
   split() {
     // ... your code goes here
+    let ms = this.getMilliseconds();
+    let s = this.getSeconds();
+    let m = this.getMinutes();
+    let instante = this.computeTwoDigitNumber(m);
+    instante += ":" + this.computeTwoDigitNumber(s);
+    instante += ":" + this.computeTwoDigitNumber(ms);
+    return instante
   }
+
 }
 
 // The following is required to make unit tests work.
--- javascript/index.js
+++ javascript/index.js
@@ -12,54 +12,136 @@ const secUniElement = document.getElementById('secUni');
 const milDecElement = document.getElementById('milDec');
 const milUniElement = document.getElementById('milUni');
 const splitsElement = document.getElementById('splits');
+//
+let corriendo = false;
 
 function printTime() {
   // ... your code goes here
+  printSeconds();
+  printMinutes();
+  printMilliseconds();
 }
 
 function printMinutes() {
   // ... your code goes here
+  //const valMin = chronometer.getMinutes();//
+  const fmto = chronometer.computeTwoDigitNumber(chronometer.getMinutes());
+  if (fmto) {
+    let u = fmto[1];
+    let d = fmto[0];
+    document.querySelector("#sphere #minDec").innerText = d;
+    document.querySelector("#sphere #minUni").innerText = u;
+  }
 }
 
 function printSeconds() {
   // ... your code goes here
+  //const valSec = chronometer.getSeconds();//
+  const fmto = chronometer.computeTwoDigitNumber(chronometer.getSeconds());
+  if (fmto) {
+    let u = fmto[1];
+    let d = fmto[0];
+    document.querySelector("#sphere #secDec").innerText = d;
+    document.querySelector("#sphere #secUni").innerText = u;
+  }
 }
 
 // ==> BONUS
 function printMilliseconds() {
-  // ... your code goes here
+  // ... your code goes her
+  const fmto = chronometer.computeTwoDigitNumber(chronometer.getMilliseconds());
+  if (fmto) {
+    let u = fmto[1];
+    let d = fmto[0];
+    if (document.querySelector("#sphere #milDec").innerText != d){
+      document.querySelector("#sphere #milDec").innerText = d;
+    }
+    if (document.querySelector("#sphere #milUni").innerText  !=  u){
+      document.querySelector("#sphere #milUni").innerText = u;
+    }
+  }
 }
 
 function printSplit() {
   // ... your code goes here
+
 }
 
 function clearSplits() {
   // ... your code goes here
+  let s = document.querySelectorAll("#splits li");
+  if (s){
+    let count=s.length
+    for (i=count-1;i>-1;i--){
+      s[i].closest("#splits").removeChild(s[i]);
+    }
+      
+    //s.children.clear;
+  }
 }
 
 function setStopBtn() {
   // ... your code goes here
+  btnLeftElement.className = 'btn start';
+  btnLeftElement.innerText = 'START';
+  btnRightElement.className = 'btn reset';
+  btnRightElement.innerText = 'RESET';
+  chronometer.stop();
 }
 
 function setSplitBtn() {
   // ... your code goes here
+  let spl = document.querySelector('#splits');
+  if (spl) {
+    let texto = chronometer.split();
+    let lix = document.createElement('li');
+    lix.innerText = texto;
+    spl.appendChild(lix);
+  }
 }
 
 function setStartBtn() {
   // ... your code goes here
+  btnLeftElement.className = "btn stop";
+  btnLeftElement.innerText = "STOP";
+  btnRightElement.className = "btn split";
+  btnRightElement.innerText = "SPLIT";
+  chronometer.start(printTime);
 }
 
 function setResetBtn() {
   // ... your code goes here
+  chronometer.reset();
+  printTime();
+  clearSplits();
 }
-
+function ctlBtnRight() {
+  if (btnRightElement.className === "btn split") {
+    setSplitBtn();
+  } else if (btnRightElement.className === "btn reset") {
+    setResetBtn();
+  }
+}
+function ctlBtnLeft() {
+  if (btnLeftElement.className === "btn stop") {
+    setStopBtn();
+  } else if (btnLeftElement.className === "btn start") {
+    setStartBtn();
+  }
+}
 // Start/Stop Button
 btnLeftElement.addEventListener('click', () => {
   // ... your code goes here
+  return ctlBtnLeft();
 });
 
 // Reset/Split Button
 btnRightElement.addEventListener('click', () => {
   // ... your code goes here
+  return ctlBtnRight();
 });
+
+window.addEventListener('load', () => {
+
+});
+
